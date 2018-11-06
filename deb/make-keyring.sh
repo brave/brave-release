53711b754d57b04057d905e7fa16e06923995657
Ben Kero <ben.kero@gmail.com>
2018-11-05 16:27:45 -0800

    deb: initial deb generation

---
+++ deb/make-keyring.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eux
+
+export BASE=$(pwd)/brave-release
+
+export GNUPGHOME=${BASE}/etc/apt/trusted.gpg.d
+
+#wget https://brave-browser-apt-release.s3.brave.com/brave-old.asc
+wget https://brave-browser-apt-release.s3.brave.com/brave-core.asc
+#gpg --no-default-keyring --keyring brave.gpg --import brave-old.asc
+gpg --no-default-keyring --keyring brave.gpg --import brave-core.asc
+rm -f ${BASE}/etc/apt/trusted.gpg.d/trustdb.gpg \
+      ${BASE}/etc/apt/trusted.gpg.d/secring.gpg \
+      brave-core.asc
