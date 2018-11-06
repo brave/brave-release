f7831b29cd57c31cea5da859db198d0b11d0a9c7
Ben Kero <ben.kero@gmail.com>
2018-11-06 08:26:05 -0800

    deb: split out releases per-file, use deb822 style format

--- deb/make-dists.sh
+++ deb/make-dists.sh
@@ -8,7 +8,9 @@ mkdir -p output
 
 for dist in xenial yakkety zesty artful bionic; do
     cp -r brave-release-base brave-release-$dist
-    sed -i "s/RELEASE/$dist/" brave-release-$dist/etc/apt/sources.list.d/brave.list
+    sed -i "s/RELEASE/$dist/" brave-release-$dist/etc/apt/sources.list.d/brave-release.sources
+    sed -i "s/RELEASE/$dist/" brave-release-$dist/etc/apt/sources.list.d/brave-beta.sources
+    sed -i "s/RELEASE/$dist/" brave-release-$dist/etc/apt/sources.list.d/brave-nightly.sources
     dpkg-deb --build brave-release-$dist output/brave-release-$dist.deb
 	rm -rf brave-release-$dist
 done
