e86b9a027bc6b0b31c43dbafc07162d0910da8a3
Ben Kero <ben.kero@gmail.com>
2018-11-06 07:32:01 -0800

    deb: output packages to output/ directory

--- deb/make-dists.sh
+++ deb/make-dists.sh
@@ -4,11 +4,12 @@ set -eux
 
 BASE="$(pwd)/brave-release-base"
 export BASE
+mkdir -p output
 
 for dist in xenial yakkety zesty artful bionic; do
     cp -r brave-release-base brave-release-$dist
     sed -i "s/RELEASE/$dist/" brave-release-$dist/etc/apt/sources.list.d/brave.list
-    dpkg-deb --build brave-release-$dist brave-release-$dist.deb
+    dpkg-deb --build brave-release-$dist output/brave-release-$dist.deb
 	rm -rf brave-release-$dist
 done
 
