3713c22ea02aad0366893fbefe31e345bb68026d
Ben Kero <bkero@bke.ro>
2019-04-09 13:36:38 -0700

    deb: refactor
    This commit does many things, including:
    
    * Moving the base dir from brave-release-base to brave-keyring
    * Adding a preinst script to remove old keys
    * Add a (new, unused) package building for 'stable' suite
    * Move the key to a keyring and place it in /usr/share/keyrings/

--- deb/make-dists.sh
+++ deb/make-dists.sh
@@ -2,16 +2,32 @@
 
 set -eux
 
-BASE="$(pwd)/brave-release-base"
+BASE="$(pwd)/brave-keyring"
 export BASE
 mkdir -p output
 
+
+########################################################################
+# Use this section when we move to a single suite for all our releases #
+########################################################################
+
+#cp -r brave-keyring brave-keyring
+#sed -i "s/RELEASE/stable/" brave-keyring/etc/apt/sources.list.d/brave-keyring.sources
+#sed -i "s/RELEASE/stable/" brave-keyring/etc/apt/sources.list.d/brave-beta.sources
+#sed -i "s/RELEASE/stable/" brave-keyring/etc/apt/sources.list.d/brave-nightly.sources
+#dpkg-deb --build brave-keyring output/brave-keyring.deb
+#rm -rf brave-keyring
+
+#############################################################################
+# Use this section when we need to make a single package per distro release #
+#############################################################################
+
 for dist in xenial yakkety zesty artful bionic; do
-    cp -r brave-release-base brave-release-$dist
-    sed -i "s/RELEASE/$dist/" brave-release-$dist/etc/apt/sources.list.d/brave-release.sources
-    sed -i "s/RELEASE/$dist/" brave-release-$dist/etc/apt/sources.list.d/brave-beta.sources
-    sed -i "s/RELEASE/$dist/" brave-release-$dist/etc/apt/sources.list.d/brave-nightly.sources
-    dpkg-deb --build brave-release-$dist output/brave-release-$dist.deb
-	rm -rf brave-release-$dist
+    cp -r "${BASE}" "${BASE}-$dist"
+    sed -i "s/RELEASE/$dist/" "${BASE}-$dist/etc/apt/sources.list.d/brave-release.sources"
+    sed -i "s/RELEASE/$dist/" "${BASE}-$dist/etc/apt/sources.list.d/brave-beta.sources"
+    sed -i "s/RELEASE/$dist/" "${BASE}-$dist/etc/apt/sources.list.d/brave-nightly.sources"
+    dpkg-deb --build "${BASE}-$dist" output/brave-keyring-$dist.deb
+	rm -rf brave-keyring-$dist
 done
 
--- deb/make-keyring.sh
+++ deb/make-keyring.sh
@@ -2,14 +2,8 @@
 
 set -eux
 
-export BASE=$(pwd)/brave-release-base
+export BASE=$(pwd)/brave-keyring
 
-export GNUPGHOME=${BASE}/etc/apt/trusted.gpg.d
-
-#wget https://brave-browser-apt-release.s3.brave.com/brave-old.asc
 wget https://brave-browser-apt-release.s3.brave.com/brave-core.asc
-#gpg --no-default-keyring --keyring brave.gpg --import brave-old.asc
-gpg --no-default-keyring --keyring brave.gpg --import brave-core.asc
-rm -f ${BASE}/etc/apt/trusted.gpg.d/trustdb.gpg \
-      ${BASE}/etc/apt/trusted.gpg.d/secring.gpg \
-      brave-core.asc
+gpg --no-default-keyring --keyring "${BASE}/usr/share/keyrings/brave-keyring.gpg" --import brave-core.asc
+rm -f brave-core.asc "${BASE}/usr/share/keyrings/brave-keyring.gpg~"
