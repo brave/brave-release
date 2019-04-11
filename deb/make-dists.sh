9144f26f4d2cd43d099ab22c28df158f633e922e
Ben Kero <bkero@bke.ro>
2019-04-11 15:36:30 -0700

    deb: empty conffiles, add postinst, simplify make-dists.sh

--- deb/make-dists.sh
+++ deb/make-dists.sh
@@ -12,22 +12,22 @@ mkdir -p output
 ########################################################################
 
 #cp -r brave-keyring brave-keyring
-#sed -i "s/RELEASE/stable/" brave-keyring/etc/apt/sources.list.d/brave-keyring.sources
-#sed -i "s/RELEASE/stable/" brave-keyring/etc/apt/sources.list.d/brave-beta.sources
-#sed -i "s/RELEASE/stable/" brave-keyring/etc/apt/sources.list.d/brave-nightly.sources
-#dpkg-deb --build brave-keyring output/brave-keyring.deb
+#sed -i "s/RELEASE/stable/" brave-keyring/etc/apt/sources.list.d/brave-keyring.sources # Uncomment when sources added to package
+#sed -i "s/RELEASE/stable/" brave-keyring/etc/apt/sources.list.d/brave-beta.sources # Ditto
+#sed -i "s/RELEASE/stable/" brave-keyring/etc/apt/sources.list.d/brave-nightly.sources # Ditto
+dpkg-deb --build brave-keyring output/brave-keyring.deb
 #rm -rf brave-keyring
 
 #############################################################################
 # Use this section when we need to make a single package per distro release #
 #############################################################################
 
-for dist in xenial yakkety zesty artful bionic; do
-    cp -r "${BASE}" "${BASE}-$dist"
-    sed -i "s/RELEASE/$dist/" "${BASE}-$dist/etc/apt/sources.list.d/brave-release.sources"
-    sed -i "s/RELEASE/$dist/" "${BASE}-$dist/etc/apt/sources.list.d/brave-beta.sources"
-    sed -i "s/RELEASE/$dist/" "${BASE}-$dist/etc/apt/sources.list.d/brave-nightly.sources"
-    dpkg-deb --build "${BASE}-$dist" output/brave-keyring-$dist.deb
-	rm -rf brave-keyring-$dist
-done
+#for dist in trusty xenial yakkety zesty artful bionic; do
+#    cp -r "${BASE}" "${BASE}-$dist"
+#    #sed -i "s/RELEASE/$dist/" "${BASE}-$dist/etc/apt/sources.list.d/brave-release.sources" # Uncomment when sources added to package
+#    #sed -i "s/RELEASE/$dist/" "${BASE}-$dist/etc/apt/sources.list.d/brave-beta.sources" # Ditto
+#    #sed -i "s/RELEASE/$dist/" "${BASE}-$dist/etc/apt/sources.list.d/brave-nightly.sources" # Ditto
+#    dpkg-deb --build "${BASE}-$dist" output/brave-keyring-$dist.deb
+#	rm -rf brave-keyring-$dist
+#done
 
