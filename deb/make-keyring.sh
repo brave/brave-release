ceef11b16f10d3b9565a9a7c3d67dc81bfa87e9f
Ben Kero <bkero@bke.ro>
2019-10-17 15:41:55 -0700

    deb: bash linting fix, wget output consistency, remove extraneous clean

--- deb/make-keyring.sh
+++ deb/make-keyring.sh
@@ -2,13 +2,13 @@
 
 set -eux
 
-export BASE=$(pwd)/brave-keyring
+BASE=$(pwd)/brave-keyring
 
-wget https://brave-browser-apt-release.s3.brave.com/brave-core.asc
+wget https://brave-browser-apt-release.s3.brave.com/brave-core.asc -O brave-core.asc
 
 # Uncomment when/if we decide to move the keyring file to /usr/share/keyrings/ as recommended in:
 # https://wiki.debian.org/DebianRepository/UseThirdParty
 
 gpg --no-default-keyring --keyring ./tmp.gpg --import brave-core.asc
 gpg --no-default-keyring --keyring ./tmp.gpg --export > "${BASE}/etc/apt/trusted.gpg.d/brave-browser-release.gpg"
-rm -f tmp.gpg~ tmp.gpg brave-core.asc brave-core-nightly.asc "${BASE}/etc/apt/trusted.gpg.d/brave-browser-release.gpg~"
+rm -f tmp.gpg~ tmp.gpg brave-core.asc "${BASE}/etc/apt/trusted.gpg.d/brave-browser-release.gpg~"
