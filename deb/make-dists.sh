3bd331ac264d5fea2dd9586eee6987d91a9097bd
Ben Kero <bkero@bke.ro>
2019-10-18 10:53:41 -0700

    deb: Add fakeroot to fix ownership of files
    Fixes: https://github.com/brave/brave-browser/issues/6230

--- deb/make-dists.sh
+++ deb/make-dists.sh
@@ -3,5 +3,5 @@
 set -eux
 
 VERSION=$(cat brave-keyring/DEBIAN/control |grep "^Version"|awk '{ print $2 }')
-dpkg-deb --build brave-keyring output/brave-keyring_$VERSION-1.deb
+fakeroot dpkg-deb --build brave-keyring output/brave-keyring_$VERSION-1.deb
 chown -R $TARGET_UID:$TARGET_GID output/
