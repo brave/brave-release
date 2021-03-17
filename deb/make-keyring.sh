541b46be218235b96dd8f7b345a13d28f40f6bb0
Wojciech Knapik <wmknapik@gmail.com>
2021-03-17 14:49:53 +0100

    Added /usr/share/keyrings/brave-browser-release.asc and added removal of /etc/apt/trusted.gpg.d/brave-browser-release.gpg in postinst if the new keyring is already in use.

--- deb/make-keyring.sh
+++ deb/make-keyring.sh
@@ -2,21 +2,10 @@
 
 set -eux
 
-BASE=$(pwd)/brave-keyring
-KEY_URL=${KEY_URL:-https://brave-browser-apt-release.s3.brave.com/brave-core.asc}
+BASE="$PWD/brave-keyring"
+KEY_URL="${KEY_URL:-https://brave-browser-apt-release.s3.brave.com/brave-core.asc}"
 
-if [ ! -f ./brave-core.asc ]; then
-  echo "No public key file (./brave-core.asc) detected in present working directory. Downloading)"
-  wget "$KEY_URL" -O brave-core.asc
-fi
+mkdir -p "${BASE:?}/etc/apt/trusted.gpg.d" "$BASE/usr/share/keyrings"
 
-# Uncomment when/if we decide to move the keyring file to /usr/share/keyrings/ as recommended in:
-# https://wiki.debian.org/DebianRepository/UseThirdParty
-
-if [ ! -d "${BASE}/etc/apt/trusted.gpg.d/" ]; then
-  mkdir -p "${BASE}/etc/apt/trusted.gpg.d/"
-fi
-
-gpg --no-default-keyring --keyring ./tmp.gpg --import brave-core.asc
-gpg --no-default-keyring --keyring ./tmp.gpg --export > "${BASE}/etc/apt/trusted.gpg.d/brave-browser-release.gpg"
-rm -f tmp.gpg~ tmp.gpg brave-core.asc "${BASE}/etc/apt/trusted.gpg.d/brave-browser-release.gpg~"
+wget -qO- "${KEY_URL:?}"|tee "$BASE/usr/share/keyrings/brave-browser-release.asc"|\
+    gpg --dearmor >"${BASE}/etc/apt/trusted.gpg.d/brave-browser-release.gpg"
