619d27e839b7874f8a3945bab9fa562b036dddd6
Wojciech Knapik <wmknapik@gmail.com>
2021-03-22 16:46:34 +0100

    Switched to the symlink approach instead of conffiles.

--- deb/make-keyring.sh
+++ deb/make-keyring.sh
@@ -8,4 +8,4 @@ KEY_URL="${KEY_URL:-https://brave-browser-apt-release.s3.brave.com/brave-core.as
 mkdir -p "${BASE:?}/etc/apt/trusted.gpg.d" "$BASE/usr/share/keyrings"
 
 wget -qO- "${KEY_URL:?}"|tee "$BASE/usr/share/keyrings/brave-browser-release.asc"|\
-    gpg --dearmor >"${BASE}/etc/apt/trusted.gpg.d/brave-browser-release.gpg"
+    gpg --dearmor >"${BASE}/usr/share/keyrings/brave-browser-release.gpg"
