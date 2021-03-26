ee9d91a0ff914dadcc15799137bad52ca69ad915
Wojciech Knapik <wmknapik@gmail.com>
2021-03-26 14:26:28 +0100

    Switched to using new keys.

--- deb/make-keyring.sh
+++ deb/make-keyring.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
-
-set -eux
+#!/bin/sh -e
 
 BASE="$PWD/brave-keyring"
-KEY_URL="${KEY_URL:-https://brave-browser-apt-release.s3.brave.com/brave-core.asc}"
 
-mkdir -p "${BASE:?}/etc/apt/trusted.gpg.d" "$BASE/usr/share/keyrings"
+mkdir -p "$BASE/usr/share/keyrings"
 
-wget -qO- "${KEY_URL:?}"|tee "$BASE/usr/share/keyrings/brave-browser-release.asc"|\
-    gpg --dearmor >"${BASE}/usr/share/keyrings/brave-browser-release.gpg"
+wget -q https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg \
+     -O "${BASE}/usr/share/keyrings/brave-browser-archive-keyring.gpg"
