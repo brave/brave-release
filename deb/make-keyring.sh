66267f0abeda9af8e0356a86c7d94de186070b9e
Wojciech Knapik <wmknapik@gmail.com>
2021-04-01 22:53:39 +0200

    Update for deb package to sync with new installation instructions. (#24)

--- deb/make-keyring.sh
+++ deb/make-keyring.sh
@@ -1,22 +1,8 @@
-#!/bin/sh
+#!/bin/sh -e
 
-set -eux
+BASE="$PWD/brave-keyring"
 
-BASE=$(pwd)/brave-keyring
-KEY_URL=${KEY_URL:-https://brave-browser-apt-release.s3.brave.com/brave-core.asc}
+mkdir -p "$BASE/usr/share/keyrings"
 
-if [ ! -f ./brave-core.asc ]; then
-  echo "No public key file (./brave-core.asc) detected in present working directory. Downloading)"
-  wget "$KEY_URL" -O brave-core.asc
-fi
-
-# Uncomment when/if we decide to move the keyring file to /usr/share/keyrings/ as recommended in:
-# https://wiki.debian.org/DebianRepository/UseThirdParty
-
-if [ ! -d "${BASE}/etc/apt/trusted.gpg.d/" ]; then
-  mkdir -p "${BASE}/etc/apt/trusted.gpg.d/"
-fi
-
-gpg --no-default-keyring --keyring ./tmp.gpg --import brave-core.asc
-gpg --no-default-keyring --keyring ./tmp.gpg --export > "${BASE}/etc/apt/trusted.gpg.d/brave-browser-release.gpg"
-rm -f tmp.gpg~ tmp.gpg brave-core.asc "${BASE}/etc/apt/trusted.gpg.d/brave-browser-release.gpg~"
+wget -q https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg \
+     -O "${BASE}/usr/share/keyrings/brave-browser-archive-keyring.gpg"
