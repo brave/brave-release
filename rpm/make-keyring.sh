e83f9f8f94f0005144fa9a39c92d96a7f9a47a5b
Francois Marier <francois@brave.com>
2023-05-10 11:37:01 -0700

    Update make-keyring.sh
    Always download a fresh key

--- rpm/make-keyring.sh
+++ rpm/make-keyring.sh
@@ -2,7 +2,4 @@
 
 KEY_URL=${KEY_URL:-https://brave-browser-rpm-release.s3.brave.com/brave-core.asc}
 
-if [ ! -f brave-core.asc ]; then
-  echo "No public key file (./brave-core.asc) detected in present working directory. Downloading)"
-  wget "$KEY_URL" -O brave-core.asc
-fi
+wget "$KEY_URL" -O brave-core.asc
