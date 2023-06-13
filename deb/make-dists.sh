55ab9ca69d3925a101cc65dcc04dc80d7b7000df
Oscar A. Jara <oajara@gmail.com>
2023-06-13 11:48:20 +0200

    Change shebang and invocation method

--- deb/make-dists.sh
+++ deb/make-dists.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-
-set -eux
+#!/usr/bin/env bash
+shopt -s inherit_errexit
+set -eEo pipefail
 
 VERSION=$(cat brave-keyring/DEBIAN/control |grep "^Version"|awk '{ print $2 }')
 fakeroot dpkg-deb --build brave-keyring output/brave-keyring_$VERSION-1.deb
